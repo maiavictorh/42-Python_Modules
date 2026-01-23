d34519a1cfadb603ccbba006775a3348286fcb9d
victode- <maiavictorh@gmail.com>
2026-01-23 01:27:01 +0000

    Added subject of each module

---
+++ m01/ex4/ft_garden_security.py
@@ -0,0 +1,44 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.__height = 0
+        self.__age = 0
+        self.set_height(height)
+        self.set_age(age)
+
+    def set_height(self, height: int) -> None:
+        if height >= 0:
+            self.__height += height
+            print(f"Height updated: {self.__height}cm [OK]")
+        else:
+            print(f"Invalid operation attempted: height {height}cm [REJECTED]")
+            print("Security: Negative height rejected")
+
+    def set_age(self, age: int) -> None:
+        if age >= 0:
+            self.__age = age
+            print(f"Age updated: {self.__age} days [OK]")
+        else:
+            print(f"Invalid operation attempted: age {age} days [REJECTED]")
+            print("Security: Negative age rejected\n")
+
+    def get_height(self) -> int:
+        return self.__height
+
+    def get_age(self) -> int:
+        return self.__age
+
+    def get_info(self) -> str:
+        return f"{self.name} ({self.get_height()}cm, {self.get_age()} days)"
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+
+    print("Plant created: Rose")
+    rose = Plant("Rose", 25, 30)
+
+    print("")
+    rose.set_height(-5)
+
+    print(f"\nCurrent plant: {rose.get_info()}")
