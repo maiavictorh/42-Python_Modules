d576eaba2f5bde26be9668c6532a61161e72ba94
Victor Hugo De Sousa Maia Goncalves <victode-@c1r11s1.42lisboa.com>
2026-01-24 18:11:33 +0000

    Removed docstrings XD

--- m01/ex4/ft_garden_security.py
+++ m01/ex4/ft_garden_security.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python3
 
 class SecurePlant:
-    """
-    A plant class that stores basic information, such as name, height and age.
-    Note that this plant have methods to protect itself from undesired input.
-    """
     def __init__(self, name: str, height: int, age: int) -> None:
-        """
-        This method is a constructor, used for initializing the
-        object's attributes when created. As we need to protect the plant
-        attributes, we'll use setters as methods to validate the parameters.
-
-        :param self: refers to the object itself;
-        :param name: plant's name;
-        :type name: str
-        :param height: plant's height, in centimeters;
-        :type height: int
-        :param age: plant's age, in days;
-        :type age: int
-        """
         self.name = name
         self._height = 0
         self._age = 0
@@ -26,15 +9,6 @@ class SecurePlant:
         self.set_age(age)
 
     def set_height(self, height: int) -> None:
-        """
-        This protection method is for checking whether the height passed to
-        the plant is positive or negative. If negative, an error message is
-        displayed, if not, the height is updated.
-
-        :param self: the plant itself;
-        :param height: the intended height;
-        :type height: int
-        """
         if height >= 0:
             self._height += height
             print(f"Height updated: {self._height}cm [OK]")
@@ -43,15 +17,6 @@ class SecurePlant:
             print("Security: Negative height rejected")
 
     def set_age(self, age: int) -> None:
-        """
-        This protection method is for checking whether the age passed to
-        the plant is positive or negative. If negative, an error message is
-        displayed, if not, the age is updated.
-
-        :param self: the plant itself;
-        :param age: the intended age;
-        :type age: int
-        """
         if age >= 0:
             self._age = age
             print(f"Age updated: {self._age} days [OK]")
@@ -60,33 +25,12 @@ class SecurePlant:
             print("Security: Negative age rejected\n")
 
     def get_height(self) -> int:
-        """
-        Access and returns a protected attribute: height.
-
-        :param self: the plant itself;
-        :return: the plant's height;
-        :rtype: int
-        """
         return self._height
 
     def get_age(self) -> int:
-        """
-        Access and returns a protected attribute: age.
-
-        :param self: the plant itself;
-        :return: the plant's age;
-        :rtype: int
-        """
         return self._age
 
     def get_info(self) -> str:
-        """
-        Returns information about the plant.
-
-        :param self: the plant itself;
-        :return: a formatted string;
-        :rtype: str
-        """
         return f"{self.name} ({self.get_height()}cm, {self.get_age()} days)"
 
 
--- m01/ex5/ft_plant_types.py
+++ m01/ex5/ft_plant_types.py
@@ -1,80 +1,29 @@
 #!/usr/bin/env python3
 
 class Plant:
-    """
-    A plant class that stores basic information, such as name, height and age.
-    """
     def __init__(self, name: str, height: int, age: int) -> None:
-        """
-        This method is a constructor, used for initializing the
-        object's attributes when created.
-
-        :param self: refers to the object itself;
-        :param name: plant's name;
-        :type name: str
-        :param height: plant's height, in centimeters;
-        :type height: int
-        :param age: plant's age, in days;
-        :type age: int
-        """
         self.name = name
         self.height = height
         self.age = age
 
     def get_info(self) -> str:
-        """
-        Returns information about the plant
-        (now in a another different format :/).
-
-        :param self: the plant itself;
-        :return: a formatted string;
-        :rtype: str
-        """
         return (f"{self.name} ({type(self).__name__}):"
                 f" {self.height}cm, {self.age} days")
 
 
 class Flower(Plant):
-    """
-    A different kind of Plant, which stores more information,
-    and have it's own methods.
-    """
     def __init__(self, name: str, height: int, age: int, color: str) -> None:
-        """
-        Works just like the Plant constructor, but have an extra
-        characteristic. Note that this class inherits the Plant attributes.
-
-        :param color: flower's color;
-        :type color: str
-        """
         super().__init__(name, height, age)
         self.color = color
 
     def get_info(self) -> str:
-        """
-        Returns information about the flower,
-        but adding specific attributes.
-
-        :param self: the flower itself;
-        :return: a formatted string;
-        :rtype: str
-        """
         return f"{super().get_info()}, {self.color} color"
 
     def bloom(self) -> None:
-        """
-        Prints a message that indicates the flower is blooming.
-
-        :param self: the flower itself;
-        """
         print(f"{self.name} is blooming beautifully!")
 
 
 class Tree(Plant):
-    """
-    Another kind of plant, which also stores different information,
-    and have it's own methods.
-    """
     def __init__(
             self,
             name: str,
@@ -82,44 +31,18 @@ class Tree(Plant):
             age: int,
             trunk_diameter: int
             ) -> None:
-        """
-        Works just like the Plant constructor, but have an extra
-        characteristic. Note that this class inherits the Plant attributes.
-
-        :param trunk_diameter: tree's trunk diameter (used for calculating
-        it's shade area);
-        :type trunk_diameter: int
-        """
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
     def get_info(self) -> str:
-        """
-        Returns information about the tree,
-        but adding specific attributes.
-
-        :param self: the tree itself;
-        :return: a formatted string;
-        :rtype: str
-        """
         return f"{super().get_info()}, {self.trunk_diameter}cm diameter"
 
     def produce_shade(self) -> None:
-        """
-        Calculates and displays the shade area produced by the tree,
-        based on the trunk diameter.
-
-        :param self: the tree itself;
-        """
         shade = self.trunk_diameter * 1.56
         print(f"{self.name} provides {shade:.0f} square meters of shade")
 
 
 class Vegetable(Plant):
-    """
-    Another kind of plant, which also stores different information,
-    and have it's own methods.
-    """
     def __init__(
             self,
             name: str,
@@ -128,36 +51,14 @@ class Vegetable(Plant):
             harvest_season: str,
             nutritional_value: str
             ) -> None:
-        """
-        Works just like the Plant constructor, but have extra characteristics.
-        Note that this class inherits the Plant attributes.
-
-        :param harvest_season: vegetable's respective harvest season;
-        :type harvest_season: str
-        :param nutritional_value: vegetable's nutritional value, such as
-        vitamins, etc;
-        """
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
 
     def get_info(self) -> str:
-        """
-        Returns information about the vegetable,
-        but adding specific attributes.
-
-        :param self: the vegetable itself;
-        :return: a formatted string;
-        :rtype: str
-        """
         return f"{super().get_info()}, {self.harvest_season} harvest"
 
     def get_nutri_value(self) -> None:
-        """
-        Display information about the vegetable's nutrients.
-
-        :param self: the vegetable itself;
-        """
         print(f"{self.name} is rich in {self.nutritional_value}")
 
 
