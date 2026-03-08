d6f1a2cbd341288045b674c1826d630808087b8d
victode- <maiavictorh@gmail.com>
2026-03-08 16:50:57 +0000

    ex3 done

--- m02/ex2/ft_custom_errors.py
+++ m02/ex2/ft_custom_errors.py
@@ -12,8 +12,8 @@ class WaterError(GardenError):
     pass
 
 
-def check_plant(water_level: int) -> None:
-    if water_level < 10:
+def check_plant(humidity: int) -> None:
+    if humidity < 10:
         raise PlantError("The tomato plant is wilting!")
 
 
@@ -22,28 +22,16 @@ def check_water(water_level: int) -> None:
         raise WaterError("Not enough water in the tank!")
 
 
-def catch_plant_error():
-    try:
-        check_plant(7)
-    except PlantError as err:
-        print(f"Caught {type(err).__name__}: {err}")
-
-
-def catch_water_error():
-    try:
-        check_water(7)
-    except WaterError as err:
-        print(f"Caught {type(err).__name__}: {err}")
-
-
 if __name__ == "__main__":
     print("=== Custom Garden Errors Demo ===\n")
-    print("Testing PlantError...")
-    catch_plant_error()
-    print("\nTesting WaterError...")
-    catch_water_error()
-    print("\nTesting catching all garden errors...")
     tests = [check_plant, check_water]
+    for test in tests:
+        try:
+            test(7)
+        except (PlantError, WaterError) as err:
+            print(f"Testing {type(err).__name__}...")
+            print(f"Caught {type(err).__name__}: {err}\n")
+    print("Testing catching all garden errors...")
     for test in tests:
         try:
             test(7)
--- m02/ex3/ft_finally_block.py
+++ m02/ex3/ft_finally_block.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+
+class InvalidPlantError(Exception):
+    pass
+
+
+def water_plants(plant_list: list) -> None:
+    print("Opening watering system")
+    try:
+        for plant in plant_list:
+            if not plant:
+                raise InvalidPlantError(f"Cannot water {plant}"
+                                        " - invalid plant!")
+            print(f"Watering {plant}")
+    except InvalidPlantError as err:
+        print(f"Error: {err}")
+    finally:
+        print("Closing watering system (cleanup)")
+
+
+def test_watering_system():
+    good_list = ["tomato", "lettuce", "carrots"]
+    bad_list = ["tomato", None, "carrots"]
+
+    print("Testing normal watering...")
+    water_plants(good_list)
+    print("Watering completed successfully!\n")
+
+    print("Testing with error...")
+    water_plants(bad_list)
+
+
+if __name__ == "__main__":
+    print("=== Garden Watering System ===\n")
+    test_watering_system()
+    print("\nCleanup always happens, even with errors!")
