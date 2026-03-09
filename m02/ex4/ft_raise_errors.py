e4785c239466850f86318f5f035fb8fe379642c0
Victor Hugo De Sousa Maia Goncalves <victode-@c1r2s13.42lisboa.com>
2026-03-09 18:31:43 +0000

    fast commit

--- m02/ex4/ft_raise_errors.py
+++ m02/ex4/ft_raise_errors.py
@@ -1,1 +1,50 @@
 #!/usr/bin/env python3
+
+def check_plant_health(plant_name: str,
+                       water_level: int,
+                       sunlight_hours: int) -> str:
+    if not plant_name:
+        raise ValueError("Plant name cannot be empty!")
+    if not 1 <= water_level <= 10:
+        if water_level < 1:
+            raise ValueError(f"Water level {water_level} is too low (min 1)")
+        else:
+            raise ValueError(f"Water level {water_level} is too high (max 10)")
+    if not 2 <= sunlight_hours <= 12:
+        if sunlight_hours < 2:
+            raise ValueError(f"Sunlight hours {sunlight_hours}"
+                             " is too low (min 2)")
+        else:
+            raise ValueError(f"Sunlight hours {sunlight_hours}"
+                             " is too high (max 12)")
+    return f"Plant '{plant_name}' is healthy!"
+
+
+def test_plant_checks() -> None:
+    print("Testing good values...")
+    try:
+        print(f"{check_plant_health('tomato', 7, 10)}")
+    except ValueError as err:
+        print(f"Error: {err}\n")
+    print("\nTesting empty plant name...")
+    try:
+        print(f"{check_plant_health(None, 7, 10)}")
+    except ValueError as err:
+        print(f"Error: {err}")
+    print("\nTesting bad water level...")
+    try:
+        print(f"{check_plant_health('tomato', 15, 10)}")
+    except ValueError as err:
+        print(f"Error: {err}")
+    print("\nTesting bad sunlight hours...")
+    try:
+        print(f"{check_plant_health('tomato', 7, 0)}")
+    except ValueError as err:
+        print(f"Error: {err}")
+    finally:
+        print("\nAll error raising tests completed!")
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Health Checker ===\n")
+    test_plant_checks()
--- m02/ex5/ft_garden_management.py
+++ m02/ex5/ft_garden_management.py
@@ -1,1 +1,71 @@
 #!/usr/bin/env python3
+
+class GardenError(Exception):
+    pass
+
+
+class PlantError(GardenError):
+    pass
+
+
+class NameError(PlantError):
+    pass
+
+
+class WaterError(PlantError):
+    pass
+
+
+class SunlightError(PlantError):
+    pass
+
+
+def check_plant_health(plant_name: str,
+                       water_level: int,
+                       sunlight_hours: int) -> str:
+    if not 1 <= water_level <= 10:
+        if water_level < 1:
+            raise WaterError(f"Water level {water_level} is too low (min 1)")
+        else:
+            raise WaterError(f"Water level {water_level} is too high (max 10)")
+    if not 2 <= sunlight_hours <= 12:
+        if sunlight_hours < 2:
+            raise ValueError(f"Sunlight hours {sunlight_hours}"
+                             " is too low (min 2)")
+        else:
+            raise ValueError(f"Sunlight hours {sunlight_hours}"
+                             " is too high (max 12)")
+    return f"Plant '{plant_name}' is healthy!"
+
+
+class Plant:
+    def __init__(self, name: str, water: int, sun: int) -> None:
+        self.name = name
+        self.water = water
+        self.sun = sun
+
+
+class GardenManager:
+    def __init__(self) -> None:
+        self.plants: list[Plant] = []
+
+    def add_plant(self, plant: Plant) -> None:
+        try:
+            if not plant.name:
+                raise NameError("Plant name cannot be empty!")                
+            self.plants.append(plant)
+            print(f"Added {plant} succesfully")
+        except NameError as err:
+            print(f"Error adding plant: {err}")
+
+    def water_plants(self) -> None:
+        try:
+            for plant in self.plants:
+                if plant.water > 9:
+                    raise WaterError("Plant water level will pass the limit!")
+                plant.water += 1
+                print(f"Watering {plant.name} - success")
+        except WaterError as err:
+            print(f"Error: {err}")
+        finally:
+            print("Closing watering system (cleanup)")
