fa7d9187644073aeee74c28c96dbd99dc5719311
Victor Hugo De Sousa Maia Goncalves <victode-@c1r4s1.42lisboa.com>
2026-03-26 17:35:35 +0000

    fast commit

--- m05/ex0/stream_processor.py
+++ m05/ex0/stream_processor.py
@@ -77,7 +77,42 @@ class TextProcessor(DataProcessor):
 class LogProcessor(DataProcessor):
     def __init__(self) -> None:
         super().__init__()
-    # print("\nInitializing Log Processor...")
+        print("\nInitializing Log Processor...")
+
+    def process(self, data: Any) -> str:
+        try:
+            sep_data = data.split(":", 1)
+            if sep_data[0] == "ERROR":
+                return f"[ALERT] ERROR level detected: {sep_data[1]}"
+            if sep_data[0] == "INFO":
+                return f"[INFO] INFO level detected: {sep_data[1]}"
+            return "Unknown log level"
+        except Exception as err:
+            return f"Error in Log Processor: {err}"
+
+    def validate(self, data: Any) -> bool:
+        if data.__class__ is not str:
+            print("\33[33mValidation: Log entry must be a string\33[0m")
+            return False
+        if not data.strip():
+            print("\33[33mValidation: Empty Log entry..\33[0m")
+            return False
+        try:
+            sep_log = data.split(":", 1)
+            if len(sep_log) != 2:
+                print('\33[33mValidation: Log must be: "level: message"\33[0m')
+                return False
+            if sep_log[0] != "ERROR" and sep_log[0] != "INFO":
+                print("\33[33mValidation: Log must be:"
+                      " \"nivel: message\"\33[0m")
+                return False
+        except Exception:
+            print("\33[31mUh-oh, Something went wrong!\33[0m")
+        print("\33[32mValidation: Log entry verified\33[0m")
+        return True
+
+    def format_output(self, result: Any) -> None:
+        return super().format_output(result)
 
 
 if __name__ == "__main__":
@@ -94,8 +129,10 @@ if __name__ == "__main__":
     text_processor.validate(text_data)
     print(text_processor.format_output(text_process))
 
-    # print("\n=== Polymorphic Processing Demo ===")
+    log_data = "ERROR: Connection timeout"
+    log_processor = LogProcessor()
+    log_process = log_processor.process(log_data)
+    log_processor.validate(log_data)
+    print(log_processor.format_output(log_process))
 
-    # print("\nProcessing multiple data types through same interface...")
-
-    # print("\n Foundation systems online. Nexus ready for advanced streams.")
+    print("\nFoundation systems online. Nexus ready for advanced streams.")
--- m05/ex1/data_stream.py
+++ m05/ex1/data_stream.py
@@ -1,12 +1,46 @@
+from typing import Any, List, Dict, Union, Optional
+from abc import ABC, abstractmethod
+
+
+class DataStream(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def process_batch(self, data_batch: List[Any]) -> str:
+        pass
+
+    def filter_data(self, data_batch: List[Any],
+                    criteria: Optional[str] = None) -> List[Any]:
+        pass
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        pass
+
+
+class SensorStream(DataStream):
+    print("\nInitializing Sensor Stream...")
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        return "Processing"
+
+
+class TransactionStream(DataStream):
+    print("\nInitializing Transaction Stream...")
+
+
+class EventStream(DataStream):
+    print("\nInitializing Event Stream...")
+
+
+class StreamProcessor:
+    pass
+
 
 if __name__ == "__main__":
     print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
 
-    print("\nInitializing Sensor Stream...")
-
-    print("\nInitializing Transaction Stream...")
-
-    print("\nInitializing Event Stream...")
+    sensor_batch = {"temp": 22.5, "humidity": 65, "pressure": 1013}
 
     print("\n=== Polymorphic Stream Processing ===")
 
