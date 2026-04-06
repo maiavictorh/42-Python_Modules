e0eaa1fab401fa2a07eecea3ea72a3a8c7d3c386
Victor Maia <maiavictorh@gmail.com>
2026-04-06 21:44:27 +0100

    fast commit

--- m07/battle.py
+++ m07/battle.py
@@ -1,12 +1,18 @@
-from ex0.Creature import Flameling
+from ex0.Creature import Flameling, Pyrodon
 
 
 if __name__ == "__main__":
 
     try:
         flameling = Flameling("Flameling", "Fire")
+        pyrodon = Pyrodon("Pyrodon", "Fire/Flying")
 
         print("Testing factory")
         print(flameling.describe())
+        print(flameling.attack())
+
+        print(pyrodon.describe())
+        print(pyrodon.attack())
+
     except ValueError as err:
         print(f"\33[31mERROR: {err}\33[0m")
--- m07/ex0/Creature.py
+++ m07/ex0/Creature.py
@@ -2,16 +2,25 @@ from abc import ABC, abstractmethod
 
 
 class Creature(ABC):
-    def __init__(self, name: str, type: str | list[str]) -> None:
+    def __init__(self, name: str, creature_type: str) -> None:
         self.name = self.validate_str(name)
-        self.type = self.validate_type(type)
+        self.creature_type = self.validate_type(creature_type)
 
     @abstractmethod
-    def attack(self) -> None:
+    def attack(self) -> str:
         pass
 
     def describe(self) -> str:
-        return f"{self.name} is a {self.type} Creature"
+        format_type = ""
+
+        i = 0
+        for item in self.creature_type:
+            format_type += item
+            if i < len(self.creature_type) - 1:
+                format_type += "/"
+            i += 1
+
+        return f"{self.name} is a {format_type} Creature"
 
     @staticmethod
     def validate_str(input: str) -> str:
@@ -25,37 +34,46 @@ class Creature(ABC):
             raise ValueError("Invalid Input.")
         return input
 
-    def validate_type(self, type_input: str) -> None:
-        valid = ["Fire", "Water", "Earth", "Air", "Flying"]
+    def validate_type(self, type_input: str) -> list[str]:
+        if type(type_input) is not str or not type_input.strip():
+            raise ValueError("Invalid Input.")
 
-        for type in valid:
-            if type_input == type:
-                self.type = type_input
-            else:
-                raise ValueError("Invalid type.")
+        valid_list = ["Fire", "Water", "Earth", "Air", "Flying"]
+        type_list = []
+
+        multiple_inputs = type_input.split("/")
+        for input in multiple_inputs:
+            clean_input = input.strip()
+            if clean_input not in valid_list:
+                raise ValueError("Invalid Type.")
+            type_list.append(clean_input)
+        return type_list
 
 
 class Flameling(Creature):
-    def __init__(self, name: str, type: str) -> None:
-        super().__init__(name, type)
+    def __init__(self, name: str, creature_type: str) -> None:
+        super().__init__(name, creature_type)
 
-    def attack(self) -> None:
-        pass
+    def attack(self) -> str:
+        return f"{self.name} uses Ember!"
 
     def describe(self) -> str:
         return super().describe()
 
 
 class Pyrodon(Creature):
-    def __init__(self, name: str, type: str) -> None:
-        super().__init__(name, type)
+    def __init__(self, name: str, creature_type: str) -> None:
+        super().__init__(name, creature_type)
+
+    def attack(self) -> str:
+        return f"{self.name} uses Flamethrower!"
 
 
 class Aquabub(Creature):
-    def __init__(self, name: str, type: str) -> None:
-        super().__init__(name, type)
+    def __init__(self, name: str, creature_type: str) -> None:
+        super().__init__(name, creature_type)
 
 
 class Torragon(Creature):
-    def __init__(self, name: str, type: str) -> None:
-        super().__init__(name, type)
+    def __init__(self, name: str, creature_type: str) -> None:
+        super().__init__(name, creature_type)
