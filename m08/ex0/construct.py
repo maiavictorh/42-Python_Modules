99ca4bc8de32abd806564492bd545fbe3d1e10c4
Victor Hugo De Sousa Maia Goncalves <victode-@c1r11s1.42lisboa.com>
2026-04-17 19:51:35 +0100

    fast commit

--- m08/ex0/construct.py
+++ m08/ex0/construct.py
@@ -1,4 +1,35 @@
 import sys
 
 if __name__ == "__main__":
-    print("\nMATRIX STATUS:", end="")
+
+    path = sys.prefix
+    default_path = sys.base_prefix
+    print("\nMATRIX STATUS: ", end="")
+
+    if path == default_path:
+        print("You're still plugged in")
+        print("\nCurrent Python:", path)
+        print("Virtual Environment: None detected")
+
+        print("\n\33[93mWARNING: You're in the global environment!\33[0m")
+        print("The machines can see everything you install.")
+
+        print("\nTo enter the construct, run:")
+        print("python -m venv matrix_env")
+        print("source matrix_env/bin/activate # On Unix")
+        print("matrix_env\\Scripts\\activate # On Windows")
+
+        print("\nThen run this program again.")
+
+    elif path != default_path:
+        print("Welcome to the construct")
+
+        split_path = path.split("/")
+        print("\nCurrent Python:", sys.executable)
+        print("Virtual Environment:", split_path[-1])
+        print("Environment Path:", path)
+
+        print("\n\33[92mSUCCESS: You're in an isolated environment!\33[0m")
+        print("Safe to install packages without affecting the global system.")
+
+        print("\nPackage installation path:")
